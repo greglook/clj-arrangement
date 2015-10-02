23324e2f9adfc8843dd82a7c3eb9d6ac700b91ca
Greg Look <greg@greg-look.net>
2015-10-01 17:38:16 -0700

    Project skeleton.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject mvxcvi/arrangement "0.1.0-SNAPSHOT"
+  :description "Total-order comparator for Clojure."
+  :url "https://github.com/greglook/clj-arrangement"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/"}
+
+  :deploy-branches ["master"]
+
+  :dependencies [[org.clojure/clojure "1.6.0"]])
