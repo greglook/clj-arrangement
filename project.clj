5d9a9cf5dd721a595f9e4211dc85fddda612c7fe
Greg Look <greg@greg-look.net>
2016-03-09 15:58:43 -0800

    Set release version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/arrangement "1.1.0-SNAPSHOT"
+(defproject mvxcvi/arrangement "1.1.0"
   :description "Total-order comparator for Clojure(Script)."
   :url "https://github.com/greglook/clj-arrangement"
   :license {:name "Public Domain"
